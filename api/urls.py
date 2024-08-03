42270752789408f27dc3ca028f6e5d71eb1b4d18
Fahad Noor <130659011+fahadnoor691@users.noreply.github.com>
2024-08-03 18:28:43 +0500

    Empty

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('create/', create_user),
-    path("", all_users),
-    path("<int:pk>/", get_user),
-    path("<int:pk>/update/", update_user),
-]  
